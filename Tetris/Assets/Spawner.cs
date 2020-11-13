aafe98e129b86bba256dce2124d0be29b0621e93
Ksenia <ksenia.shinkorenko@gmail.com>
2020-11-13 12:13:29 +0300

    Add project

---
+++ Tetris/Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Groups
+    public GameObject[] groups;
+
+    public void spawnNext()
+    {
+        //Random Index
+        int i = UnityEngine.Random.Range(0, groups.Length);
+
+        //Spawn Group at current position
+        Instantiate(groups[i],
+            transform.position,
+            Quaternion.identity);
+    }
+    void Start()
+    {
+        spawnNext();
+    }
+}
